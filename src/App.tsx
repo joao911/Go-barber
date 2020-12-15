8d169304243366824ea7fb1cbce9a61af08d56ac
joao911 <joaopaulolacerda911@gmail.com>
2020-12-14 21:01:49 -0300

    test

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+import Signin from './pages/Signin'
+function App() {
+  return (
+    <>
+      <Signin />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
